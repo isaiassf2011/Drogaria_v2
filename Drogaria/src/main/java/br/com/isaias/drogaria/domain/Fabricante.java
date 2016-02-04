f7b3f90efa2faeb55c1a6db73578b17a8e0045dd
brd03-pc <brd03-pc@brd03-pc>
2016-02-04 18:26:40 -0200

    Alterações

---
+++ Drogaria/src/main/java/br/com/isaias/drogaria/domain/Fabricante.java
@@ -0,0 +1,21 @@
+package br.com.isaias.drogaria.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@SuppressWarnings("serial")
+@Entity
+public class Fabricante extends GenericDomain {
+	
+	@Column(length = 50, nullable = false)
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
