3be489d84426a0f7739d23fc0d79ca68e0d4ad56
brd03-pc <brd03-pc@brd03-pc>
2016-02-18 18:04:46 -0200

    Alterações

--- Drogaria/src/main/java/br/com/isaias/drogaria/bean/EstadoBean.java
+++ Drogaria/src/main/java/br/com/isaias/drogaria/bean/EstadoBean.java
@@ -1,15 +1,45 @@
 package br.com.isaias.drogaria.bean;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
+import br.com.isaias.drogaria.dao.EstadoDAO;
+import br.com.isaias.drogaria.domain.Estado;
+
+@SuppressWarnings("serial")
 @ManagedBean
-public class EstadoBean {
+@ViewScoped
+public class EstadoBean implements Serializable {
+	
+	private Estado estado;
+	
+	public Estado getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
+	public void novo() {
+		estado = new Estado();
+	}
 	
 	public void salvar(){
-		
-		Messages.addGlobalError("Programação Web Com Java");
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.salvar(estado);
+			
+			novo();
+			Messages.addGlobalInfo("Estado salvo com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o estado");
+			erro.printStackTrace();
+		}
 		
 	}
 
